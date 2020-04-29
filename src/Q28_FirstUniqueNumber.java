74fbbd8790dd13a515061a8dc68846adc9a84dae
ramyhelow <ramyhelow97@gmail.com>
2020-04-28 18:24:27 -0700

    Submitted Q28 - 04/28/2020

---
+++ src/Q28_FirstUniqueNumber.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Q28_FirstUniqueNumber {
+
+    static class FirstUnique {
+
+        Set<Integer> queue;
+        Set<Integer> removedNumbers;
+
+        public FirstUnique(int[] nums) {
+            queue = new HashSet<>();
+            removedNumbers = new HashSet<>();
+            for (int num: nums) {
+                add(num);
+            }
+        }
+
+        public int showFirstUnique() {
+            if(queue.size()>0) return queue.iterator().next();
+            else return -1;
+        }
+
+        public void add(int value) {
+            if(!removedNumbers.contains(value)){
+                if(!queue.contains(value)){
+                    queue.add(value);
+                }else{
+                    queue.remove(value);
+                    removedNumbers.add(value);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FirstUnique firstUnique = new FirstUnique(new int[]{7,7,7,7,7,7});
+        System.out.println(firstUnique.showFirstUnique());
+        firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7]
+        firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3]
+        firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3,3]
+        firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7,3,3,7]
+        firstUnique.add(17);           // the queue is now [7,7,7,7,7,7,7,3,3,7,17]
+        System.out.println(firstUnique.showFirstUnique());
+    }
+
+}
